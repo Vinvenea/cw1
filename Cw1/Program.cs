0214527b25969de20365e98e9e3df88ab2984e1c
vinvenea <s18291@pjwstk.edu.pl>
2020-03-09 23:11:38 +0100

    Pkt 4 Exception

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -15,15 +15,30 @@ namespace Cw1
                 // Console.WriteLine("Hello World!");
 
                 var newPerson = new Person { FirstName = "Daniel" };
-                var url = args.Length > 0 ? args[0] : "https://pja.edu.pl";
+                var url = args.Length > 0 ? args[0] : throw new ArgumentNullException();
+
+                var regexURL = new Regex("https/*");
+
+
+                if (!regexURL.IsMatch(url))
+                {
+                    throw new ArgumentException();
+                }
 
 
                 using (var Client = new HttpClient())
                 {
                     var response = await Client.GetAsync(url);
+                    Console.WriteLine(response + "MOJ RESPONSE");
+                    var statusCode = new Regex("Status Code: 2[0-9][0-9]*");
+                    if (!statusCode.IsMatch(response.ToString()))
+                    {
+                        throw new Exception("Blad w pobieraniu strony");
+                    }
                     if (response.IsSuccessStatusCode)
                     {
                         var htmlContent = await response.Content.ReadAsStringAsync();
+                       
                         var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
 
                         var matches = regex.Matches(htmlContent);
@@ -43,6 +58,15 @@ namespace Cw1
                 Console.WriteLine(e.Message);
 
             }
+            catch(ArgumentNullException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
